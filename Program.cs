a7246d78ced6d4257cf493a04db617e4f53d4d68
styazhkoV <alexsandr.styazhkov@gmail.com>
2021-04-05 22:40:18 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BP1_1_1_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
